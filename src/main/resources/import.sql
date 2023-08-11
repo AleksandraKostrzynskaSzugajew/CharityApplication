e1f8d743434fa1c3143dc8a102bc5f4895fbb8ea
AleKosSzu <kostrzynskaaleksandra@gmail.com>
2023-08-11 10:50:50 +0200

    database transferred to AWS cloud

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
+use charity_donation;
+
 insert into institution (name, description) values ("Anioly","Anioły są wśród Nas - Przyjdź, Zobacz, Pomóż i Pozostań z Nami");
 insert into institution (name, description) values ("Czerwone noski","Clown w szpitalu");
 insert into institution (name, description) values ("Fundacja Anny Dymnej","Warto mimo wszystko!");
@@ -6,15 +8,8 @@ insert into institution (name, description) values ("Fundacja Nauron+","Dostrzeg
 insert into institution (name, description) values ("Fundacja IRASIAD - ZAGUBIONYM","Dobroczynność nie polega na dawaniu kości psu");
 
 
-insert into donation (city, pick_up_comment, pick_up_date, pick_up_time, quantity, street, zip_code) VALUEs ("Warszawa" , "ostroznie szklo", "2023-11-16", "17:00", 3, "Warszawska", "08-263") ;
 
 insert into category (name) value ("Zabawki");
 insert into category (name) value ("Ubrania");
 insert into category (name) value ("Buty");
 insert into category (name) value ("Inne");
-
-insert into user ( email, password) values ( "guest@wp.pl", "$2a$10$zzlH6E3frv97E2OKO58XnuowaZz57.zGzqNoMa6CJgyT48mJjvyjq");
-insert into user ( email, password) values ("admin@wp.pl", "$2a$10$zzlH6E3frv97E2OKO58XnuowaZz57.zGzqNoMa6CJgyT48mJjvyjq");
-
-insert into role (name) values ("ROLE_ADMIN");
-insert into role (name) values ("ROLE_USER");
